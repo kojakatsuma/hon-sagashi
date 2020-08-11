f440933f357eb373e08789425cbb6e3de20b55e7
kojakatsuma <katsuma.koja@ubiregi.com>
2020-08-11 11:53:31 +0900

    update

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const { writeFileSync } = require('fs')
 const puppeteer = require('puppeteer')
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
+
 ;(async () => {
   const browser = await puppeteer.launch({
-    headless: true,
+    headless: false,
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
   })
   const page = await browser.newPage()
@@ -10,48 +13,76 @@ const puppeteer = require('puppeteer')
   const titlelist = await getItems(page)
   console.log('complete getItem')
   const books = ['## 見つかった本']
+  const maybeBooks = ['## 多分ある本']
   const unFoundBooks = ['## 見つからなかった本']
   for (const title of titlelist) {
-    console.log(`searching for ${title}`)
     const link = await search(page, title)
     if (link) {
       books.push(`- [${title}](${link})`)
-    } else {
-      unFoundBooks.push(`- ${title}`)
+      continue
     }
-    console.log(`end`)
+    const wakatiTitle = await searchForWakati(title)
+    if (wakatiTitle) {
+      const maybeLink = await search(page, wakatiTitle)
+      if (maybeLink) {
+        maybeBooks.push(`- [${title}](${maybeLink})`)
+        continue
+      }
+    }
+    unFoundBooks.push(`- ${title}`)
   }
   console.log('complete search')
-  writeFileSync('./result.md', books.concat(unFoundBooks).join('\n'), {
-    encoding: 'utf-8',
-  })
+  writeFileSync(
+    './result.md',
+    books.concat(maybeBooks).concat(unFoundBooks).join('\n'),
+    {
+      encoding: 'utf-8',
+    },
+  )
   await browser.close()
 })()
+
 const search = async (page, title) => {
   await page.goto('https://www.lib.city.ota.tokyo.jp/index.html')
+  await page.waitForSelector('.imeon')
   const search = await page.$('.imeon')
   await search.type(title)
-  await search.press('Enter')
-  await page.waitForNavigation()
+  await page.waitForSelector('input[name="buttonSubmit"]')
+  await page.click('input[name="buttonSubmit"]')
+  console.log(`searching for ${title}`)
+  await page.waitForSelector('#honbun > section')
+
+  const topLink = await page.evaluate(() => {
+    const topResult = document.querySelector('#result > section > h3 > a')
+    if (topResult) {
+      return topResult.href
+    }
+    return null
+  })
+
+  if (topLink) {
+    return topLink
+  }
+
   const suggest = await page.$(
     '#honbun > section > div > div > div > div:nth-child(6) > div > dl > dd > a',
   )
   if (suggest) {
     await suggest.click()
-    await page.waitForNavigation()
-  }
-
-  const link = await page.evaluate(() => {
-    const topResult = document.querySelector('#result > section > h3 > a')
-    if(topResult){
+    await page.waitForSelector('#honbun > section')
+    const linkOfSuggest = await page.evaluate(() => {
+      const topResult = document.querySelector('#result > section > h3 > a')
+      if (topResult) {
         return topResult.href
-    }
-    return null
-  })
-  return link
+      }
+      return null
+    })
+    return linkOfSuggest
+  }
+  return null;
 }
 
-async function getItems(page) {
+const getItems = async (page) => {
   await page.goto(
     'https://www.amazon.co.jp/hz/wishlist/ls/1LT97CIJHMD3V?ref_=wl_share',
   )
@@ -72,4 +103,13 @@ async function getItems(page) {
     return title
   })
   return titlelist
+}
+
+const searchForWakati = async (title) => {
+  const { stdout, stderr } = await exec(`echo "${title}" | mecab -Owakati`)
+  if (stderr) {
+    console.log(stderr)
+    return null
+  }
+  return stdout
 }
